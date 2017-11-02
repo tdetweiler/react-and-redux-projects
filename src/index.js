9255693c3352ea8a73b0fe6d910f78495ce5e877
Todd <t.detweiler13@gmail.com>
2017-11-02 14:32:37 -0700

    add youtube application branch

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
+const API_KEY = 'AIzaSyAH3GkD5TjSNnXYxJ1rdel89H8NIik2zmw';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+    constructor(props){
+        super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+        this.state = { 
+            videos: [], 
+            selectedVideo: null
+        };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        this.videoSearch('Super Mario Odyssey');
+    }
+
+    videoSearch(term) {
+        YTSearch({key:API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+            //when variables have the same string name ^ = this.setState({videos:videos});
+        });
+    }
+
+    render(){
+        const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
